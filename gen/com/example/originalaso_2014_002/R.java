baf35ec10874b7274b4bba63c9a2f401cd9bef94
student <student@PC111S48.fgakusei.local>
2014-06-20 11:15:09 +0900

    a

--- gen/com/example/originalaso_2014_002/R.java
+++ gen/com/example/originalaso_2014_002/R.java
@@ -26,13 +26,13 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f08000c;
-        public static final int button1=0x7f08000a;
-        public static final int button2=0x7f08000b;
+        public static final int btnDELETE=0x7f08000b;
+        public static final int btnMAINTE_BACK=0x7f080009;
         public static final int edtMsg=0x7f080003;
         public static final int hitokoto=0x7f080005;
         public static final int imageView1=0x7f080007;
         public static final int linearLayout1=0x7f080000;
-        public static final int listView1=0x7f080009;
+        public static final int lstHITOKOTO=0x7f08000a;
         public static final int mente=0x7f080004;
         public static final int textView2=0x7f080002;
         public static final int touroku=0x7f080006;
--- src/com/example/originalaso_2014_002/MaintenanceActivity.java
+++ src/com/example/originalaso_2014_002/MaintenanceActivity.java
@@ -2,11 +2,11 @@ package com.example.originalaso_2014_002;
 
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,7 +16,8 @@ import android.widget.Toast;
 
 
 
-public class MaintenanceActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener{
+public class MaintenanceActivity extends Activity implements
+	View.OnClickListener, AdapterView.OnItemClickListener{
 
 
 	SQLiteDatabase sdb = null;
@@ -31,39 +32,67 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 		setContentView(R.layout.maintenance_activity);
 	}
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		super.onResume();
+
+		Button btnDelete = (Button)findViewById(R.id.btnDELETE);
+
+		Button btnMainte_Back = (Button)findViewById(R.id.btnMAINTE_BACK);
+
+		Button lstHitokoto = (Button)findViewById(R.id.lstHITOKOTO);
+
+		btnDelete.setOnClickListener(this);
+		btnMainte_Back.setOnClickListener(this);
+
+		lstHitokoto.setOnItemClickListener(this);
+
+		this.setDBValuetoList(lstHitokoto);
+	}
+	
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long vievid) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(this.selectedID!=-1){
+			parent.getChildAt(this.lastPosition).setBackgroundColor(0);
+
+		}
+		view.setBackgroundColor(android.graphics.Color.LTGRAY);
+
+		SQLiteCursor cursor = (SQLiteCursor)parent.getItemAtPosition(position);
+		this.selectedID = cursor.getInt(cursor.getColumnIndex("_id"));
+		this.lastPosition = position;
 	}
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-	@Override
-	protected void onResume() {
-		// TODO 自動生成されたメソッド・スタブ
-		super.onResume();
-		
-		Button btnDelete = (Button)findViewById(R.id.btnDELETE);
-		
-		Button btnMainte_Back = (Button)findViewById(R.id.btnMAITE_BACK);
-		
-		Button lstHitokoto = (Button)findViewById(R.id.LvHITOKOTO);
-		
-		btnDelete.setOnClickListener(this);
-		btnMainte_Back.setOnClickListener(this);
-		
-		lstHitokoto.setOnItemClickListener(this);
-		
-		this.setDBValuetoList(lstHitokoto);
-	}
+		switch(v.getId()){
+			case R.id.btnDELETE:
 
-	private void setDBValuetoList(ListView lstHitokoto){
+			if(this.selectedID != -1){
+				this.deleteFromHitokoto(this.selectedID);
+				ListView lstHitokoto = (ListView)findViwById(R.id.LvHitokoto);
+
+				this.setDBValuetoList(lstHitokoto);
+				this.selectedID = -1;
+				this.lastPosition = -1;
+			}
+			else{
+				Toast.makeText(MaintenanceActivity.this,"削除する行を選んでください",Toast.LENGTH_SHORT).show();
+			}
+		break;
+			case R.id.btnMAINTE_BACK:
+				finish();
+				break;
+		}
+
+
+
+
+	}
 		
-		SQLiteCursor cursor = null;
-		
-		if(sdb ==null){
+	private void deleteFromHitokoto(int id){
+		if(sdb == null){
 			helper = new MySQLiteOpenHelper(getApplicationContext());
 			
 		}
@@ -71,27 +100,45 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 			sdb = helper.getWritableDatabase();
 		}catch(SQLiteException e){
 			
-			Log.e("ERROR", e.toString());
-			
+			Log.e("ERROR",e.toString());
 		}
-		
-		cursor = this.helper.selectHitokotoList(sdb);
-		
-		int db_layout = android.R.layout.simple_list_item_activated_1;
-		
-		String[]from = {"phrase"};
-		
-		int[] to = new int[]{android.R.id.text1};
-		
-		SimpleCursorAdapter adapter =
-				new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
-		
-		lstHitokoto.setAdapter(adapter);
-		
-	}
-		
-		
-}
+		this.helper.deleteHitokoto(sdb,id);
 	}
 
-}
+
+	private void setDBValuetoList(ListView lstHitokoto){
+
+		SQLiteCursor cursor = null;
+
+		if(sdb ==null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+
+			Log.e("ERROR", e.toString());
+
+		}
+
+		cursor = this.helper.selectHitokotoList(sdb);
+
+		int db_layout = android.R.layout.simple_list_item_activated_1;
+
+		String[]from = {"phrase"};
+
+		int[] to = new int[]{android.R.id.text1};
+
+		SimpleCursorAdapter adapter =
+				new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
+
+		lstHitokoto.setAdapter(adapter);
+
+	}
+
+
+
+	}
+
+
